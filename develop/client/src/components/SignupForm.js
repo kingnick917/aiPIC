1b370041d0ac24ee5a80decfdd20feebc4e128bc
nicholas mukhtar <nmukhtar2001@gmail.com>
2023-05-22 21:43:38 -0700

    moved files

--- develop/client/src/components/SignupForm.js
+++ develop/client/src/components/SignupForm.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
-
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 
