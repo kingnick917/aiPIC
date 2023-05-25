1585232ce62a8b86db09d369cea328e8d88ad152
nicholas mukhtar <nmukhtar2001@gmail.com>
2023-05-24 21:23:21 -0700

    added inportexts to app.js

--- develop/client/src/App.js
+++ develop/client/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Body from'./components/pages/body';
+import Inportexts from './components/pages/inportexts'
 import {
   ApolloClient,
   InMemoryCache,
@@ -38,6 +39,7 @@ function App() {
     <div className="App">
       <ApolloProvider client={client}>
       <Body/>
+      <Inportexts/>
       </ApolloProvider>
     </div>
   );
