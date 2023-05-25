39c3a53203883df7b67ecceac035b1243e915dcf
nicholas mukhtar <nmukhtar2001@gmail.com>
2023-05-24 19:11:13 -0700

    did work in pages and app.js

--- develop/client/src/App.js
+++ develop/client/src/App.js
@@ -1,29 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Body from'./components/pages/body';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <ApolloProvider client={client}>
-        <Router>
-          <>
-            <Navbar />
-            <Routes>
-              <Route
-                path='/'
-                element={<SearchBooks />}
-              />
-              <Route
-                path='/saved'
-                element={<SavedBooks />}
-              />
-              <Route
-                path='*'
-                element={<h1 className='display-2'>Wrong page!</h1>}
-              />
-            </Routes>
-          </>
-        </Router>
+      <Body/>
       </ApolloProvider>
     </div>
   );
---
+++ develop/client/src/components/pages/body.js
@@ -0,0 +1,14 @@
+function body(){
+
+
+return(
+<div>
+<inport></inport>
+
+
+
+</div>
+
+
+)}
+
