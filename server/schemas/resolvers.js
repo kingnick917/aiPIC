9e1ce381fb735d3a2225bec6f268f04930073b1c
nicholas mukhtar <nmukhtar2001@gmail.com>
2023-05-25 18:52:34 -0700

    did work in resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,30 @@
+const { User, } = require('../models');
+const { signToken } = require('../utils/auth')
+
+
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      return User.findOne({ _id: context.user.id });
+    },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne(email)
+      user.isCorrectPassword(password)
+      const token = signToken(user);
+      return token
+    },
+    
+    addUser: async (parent, { username, email, password }) => {
+      console.log(username, email, password)
+       const user = await User.create(username, email, password)
+       const token = signToken(user);
+       return token
+    },
+
+  },
+};
+
+module.exports = resolvers;
